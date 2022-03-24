d51a37fb5f010c969a87c17665fff1a40605dc2b
Daniel <kr.danielreis@gmail.com>
2022-03-24 20:32:14 -0300

    @DanielSantosReis [ Projeto em progresso, feito alguns components :rocket:]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo:'home', pathMatch: 'full'},
+
+  {path: 'menu', component: MenuComponent},
+  {path: 'rodape', component: RodapeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
+import { AccessComponent } from './access/access.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    LoginComponent,
+    AccessComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule // Necessário para rodar a NgModel nas Models!
   ],
   providers: [],
   bootstrap: [AppComponent]
